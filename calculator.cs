f6d268f8678e0a950f06d03275a83ec9645f8993
ahmadrizaldi <riz@students.amikom.ac.id>
2025-03-13 10:04:46 +0700

    melengkapi fungsi main

--- calculator.cs
+++ calculator.cs
@@ -14,16 +14,4 @@ class Calculator
 
 		Console.WriteLine("\nTekan sembarang key untuk keluar");
 		Console.ReadKey();
-	}
-
-	static int Penambahan(int a, int b)
-	{
-		return a + b;
-	}
-
-	static int Pengurangan(int a, int b)
-	{
-		return a - b;
-	}
-}
-
+	
